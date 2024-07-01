338f2785f1959a13c77de46efbe1f3d4bb53c9b2
Eduardo Barreira <edubarreira1212@gmail.com>
2024-07-01 11:44:10 -0300

    feat: getting rows values and pushing object to an array

--- src/index.js
+++ src/index.js
@@ -26,3 +26,21 @@ for (const user of sheetObjects) {
     const userAdded = await sheet.addRow({Email: user.Email, Name: user.Name, Password: user.Password, Phone: user.Phone, CEP: user.CEP});
     console.log(userAdded); 
 }
+
+const rows = await sheet.getRows();
+
+const users = [];
+
+for (let i = 0; i < rows.length; i++) {
+    const user = {
+        Email: rows[i].get('Email'),
+        Name: rows[i].get('Name'),
+        Password: rows[i].get('Password'),
+        Phone: rows[i].get('Phone'),
+        CEP: rows[i].get('CEP'),
+    }
+    users.push(user);
+}
+
+console.table(users);
+
