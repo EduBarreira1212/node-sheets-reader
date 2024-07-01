1b242233d5b6ec4d5e01f034d23a389c8f8418d3
Eduardo Barreira <edubarreira1212@gmail.com>
2024-07-01 10:44:21 -0300

    feat: objects to populate sheet created

---
+++ src/sheetObjects.js
@@ -0,0 +1,39 @@
+const sheetObjects = [
+    {
+        "Email": "ana@gmail.com",
+        "Name": "Ana",
+        "Password": "abcde",
+        "Phone": "(11)99876-5432",
+        "CEP": "01001000"
+    },
+    {
+        "Email": "carlos@gmail.com",
+        "Name": "Carlos",
+        "Password": "54321",
+        "Phone": "(21)12345-6789",
+        "CEP": "20040030"
+    },
+    {   
+        "Email": "maria@gmail.com",
+        "Name": "Maria",
+        "Password": "qwert",
+        "Phone": "(31)87654-3210",
+        "CEP": "30130000"
+    },
+    {
+        "Email": "joao@gmail.com",
+        "Name": "João",
+        "Password": "password",
+        "Phone": "(41)23456-7890",
+        "CEP": "80010000"
+    },
+    {
+        "Email": "lucas@gmail.com",
+        "Name": "Lucas",
+        "Password": "123abc",
+        "Phone": "(51)34567-8901",
+        "CEP": "90010000"
+    }
+]
+
+export default sheetObjects;
