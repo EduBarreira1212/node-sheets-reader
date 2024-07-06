2de1e19084236ace8eda254fd9328db66e2d277a
Eduardo Barreira <edubarreira1212@gmail.com>
2024-07-06 19:30:44 -0300

    feat: submit event for register form created

--- web/index.js
+++ web/index.js
@@ -2,6 +2,8 @@ const registerBtn = document.getElementById("register-btn");
 const searchBtn = document.getElementById("search-btn");
 const registerSec = document.getElementById("register-section");
 const searchSec = document.getElementById("search-section");
+const registerForm = document.getElementById("register-form");
+const searchForm = document.getElementById("search-form");
 
 registerBtn.addEventListener("click", () => {
     registerSec.classList.toggle("hidden");
@@ -9,4 +11,14 @@ registerBtn.addEventListener("click", () => {
 
 searchBtn.addEventListener("click", () => {
     searchSec.classList.toggle("hidden");
+});
+
+registerForm.addEventListener("submit", (event) => {
+    fetch("http://localHost:3000/api/update-sheet", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(event.target.value),
+    })
 });
