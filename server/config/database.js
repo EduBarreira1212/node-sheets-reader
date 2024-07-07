9ebfb01a6d458884cf6010b01d267da2221e8467
Eduardo Barreira <edubarreira1212@gmail.com>
2024-07-07 20:25:42 -0300

    cleanup: removing db config file

--- server/config/database.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-
-dotenv.config({path: "../.env"});
-
-const sequelize = new Sequelize(`postgresql://postgres:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:24923/railway`);
-
-export default sequelize;
