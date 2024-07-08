001fab60f67de111ea9f449406b5de08a4d47140
Eduardo Barreira <edubarreira1212@gmail.com>
2024-07-08 12:29:29 -0300

    fix: creating sequelize instance

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,10 @@
 import { DataTypes } from "sequelize";
-import sequelize from "../config/database.js";
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config({path: "../.env"});
+
+const sequelize = new Sequelize(`postgresql://postgres:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:24923/railway`);
 
 const User = sequelize.define("Users", {
     email: {
