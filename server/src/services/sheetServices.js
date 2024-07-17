f7fed1ff783875f0c5757b7cd4c86374200bccbe
Eduardo Barreira <edubarreira1212@gmail.com>
2024-07-17 12:03:21 -0300

    feat: new service created

--- server/src/services/sheetServices.js
+++ server/src/services/sheetServices.js
@@ -1,6 +1,24 @@
-const updateSheets = async (userForm) => {
+import { createNewUser } from "../repositories/sheetRepositorie.js";
+import User from "../models/User.js";
+import { sheet } from "../index.js";
+
+export const updateSheets = async (userForm) => {
     const userAdded = await sheet.addRow({Email: userForm.Email, Name: userForm.Name, Password: userForm.Password, Phone: userForm.Phone, CEP: userForm.CEP});
     return userAdded;
 }
 
-export default updateSheets;
+export const getSheets = async (rows) => {
+    await User.sync();
+
+    for (let i = 0; i < rows.length; i++) {
+        const user = {
+            email: rows[i].get('Email'),
+            name: rows[i].get('Name'),
+            password: rows[i].get('Password'),
+            phone: rows[i].get('Phone'),
+            CEP: rows[i].get('CEP'),
+        };
+        const userCreated = await createNewUser(rows[i].get('Email'), user);
+        console.log(userCreated);
+    }
+}
