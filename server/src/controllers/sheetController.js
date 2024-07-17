d28a563c6fc4c5b4be694884e6a7d504fbd176c5
Eduardo Barreira <edubarreira1212@gmail.com>
2024-07-17 12:04:27 -0300

    feat: controller file created

---
+++ server/src/controllers/sheetController.js
@@ -0,0 +1,37 @@
+import { getSheets } from "../services/sheetServices.js";
+import { updateSheets } from "../services/sheetServices.js";
+import { Sequelize } from "sequelize";
+import { sheet } from "../index.js";
+
+export const updateSheet = async (req, res) => {
+    const userForm = req.body;
+
+    const userAdded = await updateSheets(userForm);
+    
+    if (userAdded) {
+        const rows = await sheet.getRows();
+        console.log(rows.length);
+        await getSheets(rows);
+        return res.status(200).json(userForm);
+    }
+};
+
+export const getData = async (req, res) => {
+    const user = req.user;
+
+    const sequelize = new Sequelize(`postgresql://postgres:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:24923/railway`);
+
+    try {
+        await sequelize.authenticate();
+        console.log("Database conected");
+
+        console.log(user.toJSON());
+
+        res.status(200).send(user);
+    } catch (error) {
+        console.log("Error to conect/insert on database:", error);
+    } finally {
+        console.log("Close");
+        sequelize.close();
+    }
+};
