bbc387eebf8db0be73eedadf2b60392d3212a235
Eduardo Barreira <edubarreira1212@gmail.com>
2024-08-09 15:09:41 -0300

    refactor: removing and changing files

--- web/user-register/src/index.tsx
+++ web/user-register/src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UserRegister from './pages/UserRegister';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <UserRegister />
   </React.StrictMode>
 );
 
