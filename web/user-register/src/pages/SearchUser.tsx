0a4ddc139faaf3c31f9f360aa3c7204097785a08
Eduardo Barreira <edubarreira1212@gmail.com>
2024-08-09 17:45:51 -0300

    feat: styles added

--- web/user-register/src/pages/SearchUser.tsx
+++ web/user-register/src/pages/SearchUser.tsx
@@ -4,17 +4,26 @@ import { useNavigate } from 'react-router-dom'
 const SearchUser = () => {
     const navigate = useNavigate();
 
-  return (
-    <div className="flex flex-col h-screen justify-center items-center">
-        <h1>Search User</h1>
-        <form className="flex flex-col justify-center items-center">
-            <input type="email" />
-            <input type="password" />
-            <input type="submit" value="Search" />
-        </form>
-        <button onClick={() => navigate("/")}>Register New User</button>
-    </div>
-  )
+    return (
+        <div className="flex flex-col h-screen justify-center items-center gap-12 bg-gray-100">
+            <h1 className="text-4xl font-bold text-blue-600">Search User</h1>
+            <form className="flex flex-col w-80 space-y-4">
+                <label htmlFor="email" className="text-lg font-medium">E-mail</label>
+                <input type="email" className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                <label htmlFor="email" className="text-lg font-medium">Password</label>
+                <input type="password" className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                <input type="submit" value="Search" className="mt-6 bg-blue-600 text-white font-semibold py-2 rounded-lg hover:bg-blue-700 cursor-pointer transition duration-300"/>
+            </form>
+            <section className="flex flex-col gap-3 border p-5 border-gray-300 rounded-lg">
+                <p>Name:</p>
+                <p>E-mail:</p>
+                <p>Password:</p>
+                <p>Phone:</p>
+                <p>CEP:</p>
+            </section>
+            <button onClick={() => navigate("/")} className="bg-green-500 text-white font-semibold py-2 px-6 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 transition duration-300">Register New User</button>
+        </div>
+    )
 }
 
 export default SearchUser;
