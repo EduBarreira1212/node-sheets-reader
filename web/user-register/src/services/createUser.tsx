f550a7b7eb1b2e481ebb803c163a158fd08759b3
Eduardo Barreira <edubarreira1212@gmail.com>
2024-08-10 17:28:53 -0300

    feat: post function created

---
+++ web/user-register/src/services/createUser.tsx
@@ -0,0 +1,20 @@
+import axios from "axios"
+
+interface IuserForm {
+    name: string
+    email: string
+    password: string
+    phone: string
+    CEP: string
+}
+
+const createUser = async (userForm: IuserForm) => {
+    try {
+        const response = await axios.post("https://node-sheets-reader.onrender.com/api/update-sheet", userForm);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching data", error);
+    }
+}
+
+export default createUser;
