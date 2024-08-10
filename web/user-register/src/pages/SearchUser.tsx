9886e9766b79ea63c95b8be865e542ffbea14885
Eduardo Barreira <edubarreira1212@gmail.com>
2024-08-10 15:45:03 -0300

    feat: using new components

--- web/user-register/src/pages/SearchUser.tsx
+++ web/user-register/src/pages/SearchUser.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import Submit from '../components/Submit';
 
 const SearchUser = () => {
     const navigate = useNavigate();
@@ -12,7 +13,7 @@ const SearchUser = () => {
                 <input type="email" className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
                 <label htmlFor="email" className="text-lg font-medium">Password</label>
                 <input type="password" className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
-                <input type="submit" value="Search" className="mt-6 bg-blue-600 text-white font-semibold py-2 rounded-lg hover:bg-blue-700 cursor-pointer transition duration-300"/>
+                <Submit value="Search"/>
             </form>
             <section className="flex flex-col gap-3 border p-5 border-gray-300 rounded-lg">
                 <p>Name:</p>
--- web/user-register/src/pages/UserRegister.tsx
+++ web/user-register/src/pages/UserRegister.tsx
@@ -1,4 +1,6 @@
 import { useForm, SubmitHandler } from "react-hook-form";
+import Label from "../components/Label";
+import Submit from "../components/Submit";
 
 type Inputs = {
   email: string
@@ -21,46 +23,42 @@ function UserRegister() {
     <div className="flex flex-col justify-center items-center h-screen max-h-screen gap-12 bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">User Register</h1>
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-80 space-y-4">
-        <label htmlFor="name" className="text-lg font-medium">Name</label>
+        <Label>Name</Label>
         <input
           type="text"
           className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           {...register("name", { required: true })}
         />
         {errors.name && <span>This field is required</span>}
-        <label htmlFor="email" className="text-lg font-medium">Email</label>
+        <Label>E-mail</Label>
         <input
           type="email"
           className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           {...register("email", { required: true })}
         />
         {errors.email && <span>This field is required</span>}
-        <label htmlFor="password" className="text-lg font-medium">Password</label>
+        <Label>Password</Label>
         <input
           type="password"
           className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           {...register("password", { required: true })}
         />
         {errors.password && <span>This field is required</span>}
-        <label htmlFor="phone" className="text-lg font-medium">Phone</label>
+        <Label>Phone</Label>
         <input
           type="text"
           className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           {...register("phone", { required: true })}
         />
         {errors.phone && <span>This field is required</span>}
-        <label htmlFor="cep" className="text-lg font-medium">CEP</label>
+        <Label>CEP</Label>
         <input
           type="text"
           className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           {...register("CEP", { required: true })}
         />
         {errors.CEP && <span>This field is required</span>}
-        <input
-          type="submit"
-          value="Register"
-          className="mt-6 bg-blue-600 text-white font-semibold py-2 rounded-lg hover:bg-blue-700 cursor-pointer transition duration-300"
-        />
+        <Submit value="Register" />
       </form>
     </div>
 
