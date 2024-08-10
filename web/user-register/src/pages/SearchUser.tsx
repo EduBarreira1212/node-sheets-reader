dcb9cabf2df7e8fde571be24726551172eef675c
Eduardo Barreira <edubarreira1212@gmail.com>
2024-08-10 16:04:25 -0300

    feat: using hook form and new components

--- web/user-register/src/pages/SearchUser.tsx
+++ web/user-register/src/pages/SearchUser.tsx
@@ -1,19 +1,45 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import Submit from '../components/Submit';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import ErrorMessage from '../components/ErrorMessage';
+import Label from '../components/Label';
+
+interface Inputs {
+    email: string
+    password: string
+}
 
 const SearchUser = () => {
     const navigate = useNavigate();
 
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
     return (
-        <div className="flex flex-col h-screen justify-center items-center gap-12 bg-gray-100">
+        <div className="flex flex-col min-h-screen justify-center items-center gap-12 bg-gray-100">
             <h1 className="text-4xl font-bold text-blue-600">Search User</h1>
-            <form className="flex flex-col w-80 space-y-4">
-                <label htmlFor="email" className="text-lg font-medium">E-mail</label>
-                <input type="email" className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
-                <label htmlFor="email" className="text-lg font-medium">Password</label>
-                <input type="password" className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
-                <Submit value="Search"/>
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-80 space-y-4">
+                <Label>E-mail</Label>
+                <input
+                    type="email"
+                    className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    {...register("email", { required: true })}
+                />
+                {errors.email && <ErrorMessage/>}
+                <Label>Password</Label>
+                <input
+                    type="password"
+                    className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    {...register("password", { required: true })}
+                />
+                {errors.password && <ErrorMessage/>}
+                <Submit value="Search" />
             </form>
             <section className="flex flex-col gap-3 border p-5 border-gray-300 rounded-lg">
                 <p>Name:</p>
