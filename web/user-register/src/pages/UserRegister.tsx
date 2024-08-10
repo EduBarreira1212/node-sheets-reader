136bfa9d05a55cadc6ca5a20d4308ad2bb3b4cfd
Eduardo Barreira <edubarreira1212@gmail.com>
2024-08-10 15:51:35 -0300

    feat: using error component

--- web/user-register/src/pages/UserRegister.tsx
+++ web/user-register/src/pages/UserRegister.tsx
@@ -1,6 +1,7 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import Label from "../components/Label";
 import Submit from "../components/Submit";
+import ErrorMessage from "../components/ErrorMessage";
 
 type Inputs = {
   email: string
@@ -20,7 +21,7 @@ function UserRegister() {
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen max-h-screen gap-12 bg-gray-100">
+    <div className="flex flex-col justify-center items-center min-h-screen gap-12 bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">User Register</h1>
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-80 space-y-4">
         <Label>Name</Label>
@@ -29,35 +30,35 @@ function UserRegister() {
           className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           {...register("name", { required: true })}
         />
-        {errors.name && <span>This field is required</span>}
+        {errors.name && <ErrorMessage/>}
         <Label>E-mail</Label>
         <input
           type="email"
           className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           {...register("email", { required: true })}
         />
-        {errors.email && <span>This field is required</span>}
+        {errors.email && <ErrorMessage/>}
         <Label>Password</Label>
         <input
           type="password"
           className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           {...register("password", { required: true })}
         />
-        {errors.password && <span>This field is required</span>}
+        {errors.password && <ErrorMessage/>}
         <Label>Phone</Label>
         <input
           type="text"
           className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           {...register("phone", { required: true })}
         />
-        {errors.phone && <span>This field is required</span>}
+        {errors.phone && <ErrorMessage/>}
         <Label>CEP</Label>
         <input
           type="text"
           className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           {...register("CEP", { required: true })}
         />
-        {errors.CEP && <span>This field is required</span>}
+        {errors.CEP && <ErrorMessage/>}
         <Submit value="Register" />
       </form>
     </div>
