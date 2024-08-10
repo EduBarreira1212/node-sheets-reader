494627ee11ec1b85251b9d71244c2cbc25b75347
Eduardo Barreira <edubarreira1212@gmail.com>
2024-08-10 15:51:18 -0300

    feat: error component created

---
+++ web/user-register/src/components/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ErrorMessage = () => {
+  return (
+    <span className="font-bold text-red-600">This field is required</span>
+  )
+}
+
+export default ErrorMessage;
