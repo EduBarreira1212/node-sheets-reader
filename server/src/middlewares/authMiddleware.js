6e0bd39c238ead68d4e6bc044693a91b996b2026
Eduardo Barreira <edubarreira1212@gmail.com>
2024-08-14 18:24:48 -0300

    refactor: change logic to use bcrypt

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -1,10 +1,18 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 const authMiddleware = async (req, res, next) => {
     const { email, password } = req.query;
-    const user = await User.findOne({where: {email: email, password: password}});
+    
+    const user = await User.findOne({where: {email: email}});
 
-    if(user){
+    if(!user){
+        res.status(401).json({error: "Invalid user"});
+    }
+
+    const uncriptedPassword = bcrypt.compareSync(password, user.password);
+
+    if(uncriptedPassword){
         req.user = user;
         next();
     }else{
