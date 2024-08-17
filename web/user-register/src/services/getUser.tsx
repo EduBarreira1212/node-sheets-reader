0d47627e9c28d67620a8907042dcbda8c4e47e85
Eduardo Barreira <edubarreira1212@gmail.com>
2024-08-17 17:51:30 -0300

    refactor: send data in body

--- web/user-register/src/services/getUser.tsx
+++ web/user-register/src/services/getUser.tsx
@@ -7,7 +7,10 @@ interface IuserInfo {
 
 const getUser = async (userInfo: IuserInfo) => {
     try {
-        const response = await axios.get(`https://node-sheets-reader.onrender.com/api/get-data?email=${userInfo.email}&password=${userInfo.password}`);
+        const response = await axios.post(`https://node-sheets-reader.onrender.com/api/get-data`, {
+            email: userInfo.email,
+            password: userInfo.password
+        });
         return response.data;
     } catch (error) {
         console.error("Error fetching data", error)
