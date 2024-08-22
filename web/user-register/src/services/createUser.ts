0fc9c3e026b3dea04b9bcade1d2ff9f2b63f5b17
Eduardo Barreira <edubarreira1212@gmail.com>
2024-08-22 18:16:26 -0300

    refactor: throw error and change to .ts

--- web/user-register/src/services/createUser.ts
+++ web/user-register/src/services/createUser.ts
@@ -13,7 +13,7 @@ const createUser = async (userForm: IuserForm) => {
         const response = await axios.post("https://node-sheets-reader.onrender.com/api/update-sheet", userForm);
         return response.data;
     } catch (error) {
-        console.error("Error fetching data", error);
+        throw new Error();
     }
 }
 
